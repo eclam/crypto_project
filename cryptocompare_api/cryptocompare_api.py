bfedd0598e31379714b780fc223743efffdbc471
Eric Lam <eclam@sfu.ca>
2018-01-06 17:12:39 -0800

    fixed info
    fixed header -> important info

--- cryptocompare_api/cryptocompare_api.py
+++ cryptocompare_api/cryptocompare_api.py
@@ -1,8 +1,10 @@
-#  Cryptocurrency Self project by Eric Lam 
-#  License: GPL 
-#  Email: CKELAM@outlook.com 
-#  Implementing Cryptochart API 
-# http://api.cryptocoincharts.info/listCoins
+#	Cryptocurrency Self project by Eric Lam
+#	Implementing Cryptochart API 
+#	URL Link for API: 		http://api.cryptocoincharts.info/ 
+#	License:				GPL 
+#	Email: 					Ericlam2012@live.com 
+#	Python Ver:			 	Python 3 + Anaconda 
+
 
 
 import json
@@ -13,8 +15,10 @@ def data_get(url, link):
 	# url = cc_charts_path + 'listCoins'
 	url_path = url + link
 	try:
-		resp = requests.get(url_path)
+		resp = requests.get(url_path, timeout=2) #timeout subject to change
 		json_data = resp.json()
+
+
 
 	except Exception as e: 
 		print('Connection Failed')
