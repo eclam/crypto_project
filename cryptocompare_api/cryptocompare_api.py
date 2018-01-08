cdd6ae3a9d2dc7439b382d86d2184a441ec65d4f
Eric Lam <eclam@sfu.ca>
2018-01-07 21:12:09 -0800

    data_get finished
    added in error handling and created the function where it grabs the info from the api

--- cryptocompare_api/cryptocompare_api.py
+++ cryptocompare_api/cryptocompare_api.py
@@ -5,30 +5,38 @@
 #	Email: 					Ericlam2012@live.com 
 #	Python Ver:			 	Python 3 + Anaconda 
 
-
-
 import json
 import requests
-
+import sys
 
 def data_get(url, link):
-	# url = cc_charts_path + 'listCoins'
+	# (For example) url = "https://min-api.cryptocompare.com/" + 'listCoins'
 	url_path = url + link
+
 	try:
-		resp = requests.get(url_path, timeout=2) #timeout subject to change
-		json_data = resp.json()
-
-
-
-	except Exception as e: 
-		print('Connection Failed')
-		print(e)
-
+		resp = requests.get(url_path, timeout=2)
+		print(resp.raise_for_status())
+		json_data = resp.json()		
+	except requests.exceptions.HTTPError as http_err:
+		print('HTTP Error: ', http_err)
+		sys.exit(1)
+	except requests.exceptions.ConnectionError as conn_error:
+		print('Cannot Connect Error: ', conn_error)
+		# sys.exit(1)
+		data_get(url, link)
+	except requests.exceptions.Timeout as timedout_err:
+		print("Timed Out Error: ",timedout_err)
+		data_get(url, link)
+	except requests.exceptions.RequestException as err:
+		print("Error: ", err)
+		sys.exit(1)
+ 	
+	return json_data
 
 
 cc_charts_path = "https://min-api.cryptocompare.com/"
-data_get(cc_charts_path,'')
+json_data = data_get(cc_charts_path,'')
 
-# print(json_data)
+print(json_data)
 
 
