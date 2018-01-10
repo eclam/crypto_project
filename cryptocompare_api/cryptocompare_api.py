ae67d0f38188f0b66a1c16c29e75225804349047
eclam <eclam@sfu.ca>
2018-01-09 16:21:14 -0800

    coded more things
    implemented dictionary + unicode format problems + other things

--- cryptocompare_api/cryptocompare_api.py
+++ cryptocompare_api/cryptocompare_api.py
@@ -5,23 +5,21 @@
 #	Email: 					Ericlam2012@live.com 
 #	Python Ver:			 	Python 3 + Anaconda 
 
-# import json
+import json
 import requests
 import sys
 
 def data_get(url):
 	# (For example) url = "https://min-api.cryptocompare.com/" + 'listCoins'
 	try:
-		resp = requests.get(url, timeout=2)
-		print(resp.raise_for_status())
-		json_data = resp.json()
+		json_data = requests.get(url, timeout=2).json()
+
 	except requests.exceptions.HTTPError as http_err:
 		print('HTTP Error: ', http_err)
 		sys.exit(1)
 	except requests.exceptions.ConnectionError as conn_error:
 		print('Cannot Connect Error: ', conn_error)
-		# sys.exit(1)
-		data_get(url, link)
+		sys.exit(1)
 	except requests.exceptions.Timeout as timedout_err:
 		print("Timed Out Error: ",timedout_err)
 		data_get(url, link)
@@ -30,27 +28,21 @@ def data_get(url):
 		sys.exit(1)
 
 	return json_data
-	
 
 # def url_build():
 
 
 
+def coin_list(coins='all'):
+	url_path = "https://min-api.cryptocompare.com/data/all/coinlist"
 
-def coin_list():
-	# url_path = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR"
-	url_path = "https://www.cryptocompare.com/api/data/coinlist"
-	json_data = data_get(url_path)
-	return json_data
+	if coins != 'all': #finds selected cryptocurrencies from the json_data 
+		text_data = data_get(url_path)['Data']
+		text_data = {c: text_data[c] for c in coins}
+	else:
+		text_data = data_get(url_path)['Data']
 
+	return text_data
 
-
-
-# cc_charts_path = "https://min-api.cryptocompare.com/"
-# json_data = data_get(cc_charts_path,'')
-json_data = coin_list()
-
-
-print(json_data)
 
 
---
+++ cryptocompare_api/test.py
@@ -0,0 +1,49 @@
+from cryptocompare_api import coin_list, data_get
+import json
+
+# cc_charts_path = "https://min-api.cryptocompare.com/"
+# json_data = data_get_cc(cc_charts_path,'')
+
+text_data = coin_list( coins='all' ) #feed list of wanted coins through coins = []
+# for key, value in text_data.items():
+# 	print(key, value)
+# 	count+=1
+# 	print(count)
+# json_data = {c:text_data[c] for c in ['ETC','BTC', 'XRP']}
+# print(json_data)
+
+# print(json.dumps(text_data))
+keys_data = text_data.keys() # takes all the keys from said library 
+
+# for key in keys_data:
+	# print(json.dumps(text_data[key]),key)
+
+
+# url_path = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR"
+# json_data = data_get(url_path)
+# print(json_data)
+
+
+#############################################################
+#might need this code in future? 
+# def data_get_cc(url):
+# 	# (For example) url = "https://min-api.cryptocompare.com/" + 'listCoins'
+# 	try:
+# 		resp = requests.get(url, timeout=2).text.encode('utf-8')
+# 		json_data = json.loads(resp)
+
+# 	except requests.exceptions.HTTPError as http_err:
+# 		print('HTTP Error: ', http_err)
+# 		sys.exit(1)
+# 	except requests.exceptions.ConnectionError as conn_error:
+# 		print('Cannot Connect Error: ', conn_error)
+# 		sys.exit(1)
+# 	except requests.exceptions.Timeout as timedout_err:
+# 		print("Timed Out Error: ",timedout_err)
+# 		data_get(url, link)
+# 	except requests.exceptions.RequestException as err:
+# 		print("Error: ", err)
+# 		sys.exit(1)
+
+# 	return json_data
+#############################################################
